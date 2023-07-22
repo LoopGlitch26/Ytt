f17920de65fb44942fc5eb3fb64b5b143ef909b3
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-07-22 12:09:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,14 @@ from moviepy.video.io.VideoFileClip import VideoFileClip
 
 def download_and_trim_video(youtube_url, start_time, end_time):
     try:
-        # Download YouTube video using pytube
         yt = YouTube(youtube_url)
         stream = yt.streams.filter(file_extension='mp4').first()
         video_path = stream.download()
 
-        # Trim the video using moviepy
         clip = VideoFileClip(video_path).subclip(start_time, end_time)
         trimmed_path = 'trimmed_video.mp4'
         clip.write_videofile(trimmed_path, codec='libx264')
 
-        # Return the trimmed video path
         return trimmed_path
 
     except Exception as e:
@@ -22,15 +19,13 @@ def download_and_trim_video(youtube_url, start_time, end_time):
         return None
 
 def main():
-    st.title("YouTube Video Trimmer")
+    st.title("YouTube Trimmer")
 
-    # Get user input: YouTube URL, start time, and end time
     youtube_url = st.text_input("Enter the YouTube video URL:")
     start_time = st.text_input("Enter the start time (in seconds):")
     end_time = st.text_input("Enter the end time (in seconds):")
 
     if st.button("Trim and Display"):
-        # Check if input fields are not empty
         if youtube_url and start_time and end_time:
             try:
                 start_time = float(start_time)
@@ -38,10 +33,8 @@ def main():
                 trimmed_path = download_and_trim_video(youtube_url, start_time, end_time)
 
                 if trimmed_path:
-                    # Display the trimmed video
                     st.video(trimmed_path)
 
-                    # Offer the download link
                     st.markdown(f"[Download Trimmed Video]({trimmed_path})")
             except ValueError:
                 st.error("Invalid input for start or end time. Please enter a valid number.")
