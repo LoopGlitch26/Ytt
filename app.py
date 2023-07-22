93a518e67e310f4c99eb1702d81fbf71e622eb1f
Bravish Ghosh <grandson.baba.2012@gmail.com>
2023-07-22 12:05:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 from pytube import YouTube
 from moviepy.video.io.VideoFileClip import VideoFileClip
-import ffmpeg
 
 def download_and_trim_video(youtube_url, start_time, end_time):
     try:
+        # Download YouTube video using pytube
         yt = YouTube(youtube_url)
         stream = yt.streams.filter(file_extension='mp4').first()
         video_path = stream.download()
 
+        # Trim the video using moviepy
         clip = VideoFileClip(video_path).subclip(start_time, end_time)
         trimmed_path = 'trimmed_video.mp4'
         clip.write_videofile(trimmed_path, codec='libx264')
 
+        # Return the trimmed video path
         return trimmed_path
 
     except Exception as e:
@@ -27,7 +29,8 @@ def main():
     start_time = st.text_input("Enter the start time (in seconds):")
     end_time = st.text_input("Enter the end time (in seconds):")
 
-    if st.button("Trim and Download"):
+    if st.button("Trim and Display"):
+        # Check if input fields are not empty
         if youtube_url and start_time and end_time:
             try:
                 start_time = float(start_time)
@@ -35,9 +38,11 @@ def main():
                 trimmed_path = download_and_trim_video(youtube_url, start_time, end_time)
 
                 if trimmed_path:
+                    # Display the trimmed video
+                    st.video(trimmed_path)
+
                     # Offer the download link
-                    st.success("Video successfully trimmed and downloaded!")
-                    st.download_button(label="Download Trimmed Video", data=trimmed_path)
+                    st.markdown(f"[Download Trimmed Video]({trimmed_path})")
             except ValueError:
                 st.error("Invalid input for start or end time. Please enter a valid number.")
         else:
@@ -45,3 +50,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
